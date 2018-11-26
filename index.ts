64e4ba3781d72a57717addd7e213d33356a70e23
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-11-26 10:47:02 +0000

    Fix lint violations.

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-export type Record = {readonly [name: string]: string};
+export type Record = { readonly [name: string]: string };
 
 export function tableToRecords(table: ReadonlyArray<ReadonlyArray<string>>): ReadonlyArray<Record> {
-    let header = table[0];
+    const header = table[0];
 
-    let objects: {[name: string]: string}[] = [];
+    const objects: Array<{ [name: string]: string }> = [];
 
     for (let i = 1; i < table.length; ++i) {
-        let row = table[i];
-        let object: {[name: string]: string} = {};
+        const row = table[i];
+        const object: { [name: string]: string } = {};
 
         for (let j = 0; j < row.length && j < header.length; ++j) {
-            let name = header[j];
+            const name = header[j];
             object[name] = row[j];
         }
 
@@ -25,16 +25,16 @@ export function recordsToTable(records: ReadonlyArray<Record>): ReadonlyArray<Re
         return [];
     }
 
-    let headers = Object.keys(records[0]);
+    const headers = Object.keys(records[0]);
 
     return Object.freeze([headers]
-            .concat(records
-                    .map(record => headers
-                            .map(header => {
-                                if (header in record) {
-                                    return record[header];
-                                } else {
-                                    throw new Error('Inconsistent records.');
-                                }
-                            }))));
+        .concat(records
+            .map(record => headers
+                .map(header => {
+                    if (header in record) {
+                        return record[header];
+                    } else {
+                        throw new Error("Inconsistent records.");
+                    }
+                }))));
 }
